a2d78aed44d2c83b0d695f44be9268ab7c0f9b82
dahee_jang <dqheee@gmail.com>
2021-07-04 11:05:57 +0900

    LOGIN

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { loginUser } from '../../../_actions/user_action';
 
-function LoginPage() {
+function LoginPage(props) {
+    const dispatch = useDispatch(); 
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        //로그인 버튼을 누를때마다 페이지가 리프레쉬 되는 것을 막아줌
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        //redux 사용
+        dispatch(loginUser(body))
+            .then(response => {
+                if(response.payload.loginSuccess) {
+                    props.history.push('/')
+                } else {
+                    alert("Error")
+                }
+            })
+
+       
+    }
+
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height:'100vh'
+        }}>
+            <form style={{ display: 'flex', flexDirection: 'column' }}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                <br />
+                <button type="submit">
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
