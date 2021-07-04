4faefd90a9123ca0c924012ebf83c3a3869ebc6a
dahee_jang <dqheee@gmail.com>
2021-07-04 12:13:33 +0900

    Register

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 import {
-    LOGIN_USER
+    LOGIN_USER,
+    REGISTER_USER
 } from './types';
+
 export function loginUser(dataToSubmit) {
 
     const request = axios.post('/api/users/login', dataToSubmit)
@@ -11,5 +13,15 @@ export function loginUser(dataToSubmit) {
         type: LOGIN_USER,
         payload: request
     }
+}
 
+export function registerUser(dataToSubmit) {
+
+    const request = axios.post('/api/users/register', dataToSubmit)
+        .then(response => response.data)
+
+    return {
+        type: REGISTER_USER,
+        payload: request
+    }
 }
